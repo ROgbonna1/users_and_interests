fe006331a37d79d6e72d2e1ef5589c9092ceba2c
Reuben Ogbonna <reubenogbonna@gmail.com>
2019-01-13 22:49:48 +0000

    completed sinatra code challenge

---
+++ users_and_interests.rb
@@ -0,0 +1,22 @@
+require 'pry'
+require 'sinatra'
+require 'psych'
+require "sinatra/reloader"
+require "tilt/erubis"
+
+before do
+  @data = Psych.load_file("data/users.yaml")
+  @users = @data.keys.map(&:to_s)
+  @interests = @data.map { |user, user_data| user_data[:interests] }.flatten
+end
+
+get '/' do
+  @title = "Home Page"
+  erb :home
+end
+
+get '/:user' do
+  @user = params[:user]
+  @title = "#{@user.capitalize}'s Page"
+  erb :user
+end
